9e33fdad91dd6691b650db8eeaa392b06db032f7
Daniel Luke Rogers <dluke.rogers@gmail.com>
2023-06-28 18:11:06 -0500

    added in variable for inGameProgress

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let rank = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th
 
 let playerArr = [];
 
+let inGameProgress = false;
+
 // constructor function
 
 function Player(name, score) {
